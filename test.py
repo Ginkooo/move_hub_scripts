cfd183c87d7098a786967c5ed33b448ae0364e53
Piotr Czajka <czajka@protonmail.com>
2017-12-26 22:11:40 +0100

    Silly move on wsad

---
+++ test.py
@@ -0,0 +1,42 @@
+import pylgbst
+from time import sleep
+from pynput import keyboard
+from functools import partial
+
+from pylgbst import BLEConnection, MoveHub
+
+connection = BLEConnection().connect(hub_mac='00:16:53:AA:B6:AC')
+
+hub = MoveHub(connection)
+print('Wait 10 s...')
+sleep(10)
+print('Ready!')
+
+def on_press(key):
+    if key == keyboard.Key.esc:
+        print('ESC!\tAborting...')
+        raise StopIteration()
+    moves = {
+            'w': partial(hub.motor_AB.timed, 1, 0.5),
+            'a': partial(hub.motor_B.timed, 1, 0.5),
+            'd': partial(hub.motor_A.timed, 1, 0.5),
+            's': partial(hub.motor_AB.timed, -1, 0.5)
+            }
+    try:
+        moves[key.char]()
+    except:
+        print('Bad key')
+
+def on_release(key):
+    pass
+
+try:
+    with keyboard.Listener (
+            on_press=on_press,
+            on_release=on_release
+            ) as listener:
+        listener.join()
+except StopIteration:
+    pass
+while True:
+    pass
